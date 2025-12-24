eceb6eb70c543a9bf9614621567ccaa698dfee5a
dac <dac@dypm.org>
2025-12-24 14:38:24 +0530

    All .Net Assignment Uploaded

---
+++ 001DemoMVC/Controllers/EmpsController.cs
@@ -0,0 +1,51 @@
+﻿using _001DemoMVC.Models;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _001DemoMVC.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [EnableCors(PolicyName = "Policy1")]
+    public class EmpsController : ControllerBase
+    {
+        EFDBContext dbObject = new EFDBContext();
+
+        [HttpGet]
+        public IEnumerable<Emp> Get()
+        {
+            return dbObject.Emps.ToList();
+        }
+
+        [HttpGet("{id}")]
+        public Emp Get(int id)
+        {
+            return dbObject.Emps.Find(id);
+        }
+
+        
+        [HttpPost]
+        public void Post([FromBody] Emp emp)
+        {
+            dbObject.Emps.Add(emp);
+            dbObject.SaveChanges();
+        }
+
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] Emp emp)
+        {
+            Emp empToUpdate = dbObject.Emps.Find(id);
+            empToUpdate.name = emp.name;
+            empToUpdate.address = emp.address;
+            dbObject.SaveChanges();
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            Emp empToDelete = dbObject.Emps.Find(id);
+            dbObject.Emps.Remove(empToDelete);
+            dbObject.SaveChanges();
+        }
+    }
+}
